f56513e31c89fb81c7351b983b91c4b9cd364e87
Rohit Chandel <rohitldt@gmail.com>
2025-08-02 16:34:42 +0530

    favicon

--- webapp/channels/webpack.config.js
+++ webapp/channels/webpack.config.js
@@ -202,10 +202,10 @@ var config = {
         // Generate manifest.json, honouring any configured publicPath. This also handles injecting
         // <link rel="apple-touch-icon" ... /> and <meta name="apple-*" ... /> tags into root.html.
         new WebpackPwaManifest({
-            name: 'Mattermost',
-            short_name: 'Mattermost',
+            name: 'Pareza Pro',
+            short_name: 'Pareza Pro',
             start_url: '..',
-            description: 'Mattermost is an open source, self-hosted Slack-alternative',
+            description: 'Pareza Pro is a collaboration platform.',
             background_color: '#ffffff',
             inject: true,
             ios: true,
